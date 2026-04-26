08e2e4be9ecb124f57d276410b5bba6cfaa88478
Ahmetcan Çavdar <ahmetcan5841@gmail.com>
2026-04-26 23:27:51 +0300

    Create RelationCheckboxItemViewModel.cs

---
+++ VirtualMuseum.Web/Models/ViewModels/RelationCheckboxItemViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualMuseum.Web.Models.ViewModels
+{
+    public class RelationCheckboxItemViewModel
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = string.Empty;
+        public bool Selected { get; set; }
+    }
+}
