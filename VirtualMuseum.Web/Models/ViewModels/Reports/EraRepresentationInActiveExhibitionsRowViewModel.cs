7d35545b75c29721d71a1d6860905088d05c1db5
Ahmetcan Çavdar <ahmetcan5841@gmail.com>
2026-04-29 22:41:40 +0300

    Add EraRepresentationInActiveExhibitionsRowViewModel

---
+++ VirtualMuseum.Web/Models/ViewModels/Reports/EraRepresentationInActiveExhibitionsRowViewModel.cs
@@ -0,0 +1,8 @@
+namespace VirtualMuseum.Web.Models.ViewModels.Reports
+{
+    public class EraRepresentationInActiveExhibitionsRowViewModel
+    {
+        public string Era { get; set; } = string.Empty;
+        public int ArtworkCount { get; set; }
+    }
+}
